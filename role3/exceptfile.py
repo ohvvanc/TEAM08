d8217ff0b4b297adb165aa9281484c8747ade7bb
dbstjdghks03 <snu15119@snu.ac.kr>
2022-06-01 18:12:42 +0900

    SH second commit

--- role3/exceptfile.py
+++ role3/exceptfile.py
@@ -3,14 +3,22 @@ from role1.dataframe import makedataframe
 def except1(a):
     while True:
         try:
-            b = int(a)
-            if b > 0:
+            b = int(a)                                                                                                  #정수가 입력되었을 때
+            if b > 0:                                                                                                   #입력된 숫자가 양의 정수일 때
                 c = makedataframe(b)
                 return c
                 break
             else:
-                print('양의 정수를 입력하세요')
+                print('양의 정수를 입력하시요')
+                a = input("데이터의 크기를 다시 입력하시오: ")
+                continue
         except ValueError:
-            print('숫자를 입력하세요')
-            a = input("데이터의 크기를 입력하시오: ")
-            continue
+            try:
+                d = float(a)
+                print('정수를 입력하시오')
+                a = input("데이터의 크기를 다시 입력하시오: ")
+                continue
+            except ValueError:
+                print('숫자를 입력하시요')
+                a = input("데이터의 크기를 다시 입력하시오: ")
+                continue
