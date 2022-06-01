d1b8a7b28d85900a8e6ad7efd634f5eb8c60a3d8
mystic030115 <mystic030115@snu.ac.kr>
2022-06-02 01:45:07 +0900

    KH2

--- role3/logging.py
+++ role3/logging.py
@@ -2,4 +2,9 @@ import logging #logging 도구 import
 
 logger = logging.getLogger("main") #main에 대해서 logger 설정
 logger.setLevel(logging.INFO) #logging.INFO 설정
-logger.info("Info Log")
+
+formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s') #logger 출력 형식
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger. addHandler(stream_handelr) #logger 출력 3줄코딩
