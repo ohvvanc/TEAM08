c5735216a0fbe0fa3de81329c2bef618914846db
mystic030115 <mystic030115@snu.ac.kr>
2022-06-02 13:57:20 +0900

    KH4

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ import os
 import logging
 if __name__ == '__main__': #패키지 파일에서 main 파일엔 이 문장이 반드시 있어야함 여러 모듈을 가져온후 이문장 밑에 실제 실행코드 입력
     dt = except1(input("데이터 크기를 입력하시오: ")) #데이터크기를 입력받고 데이터프레임을 dt에 저장
-    logging.info("Start finding solutions...")
+    logging.basicConfig(level=logging.INFO)
     print("---------Dataframe-----------")
+    logging.info("Making Dataframe...")
+
     print(dt)
     print("----------Solution-----------")
+    logging.info("Start finding solutions...")
+
     print(findsolution(dt)) #findsolution 함수에 dt 입력
     if not os.path.isdir("output"): #output 이라는 폴더에 csv파일을 저장하기위해 폴더가 없을 경우 폴더 생성
         os.mkdir("output")
